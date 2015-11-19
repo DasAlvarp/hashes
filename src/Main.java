51fdf9eafae367908b904025c1c780eba07fa83a
DasAlvarp <alvaroguis@gmail.com>
2015-11-19 13:32:42 -0600

    output fancification

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 /**
  * Created by alvarpq on 18/11/15.
+ *
  */
 public class Main
 {
@@ -7,12 +8,12 @@ public class Main
     {
 
         //making stuff happen. Reset it a bunch to get all the things.
-        System.out.println("60%");
+        System.out.print("60%");
         AlvarHash testHash = new AlvarHash(1019, false, 611, 10000);
         System.out.println(testHash.probeAndResults());
         testHash = new AlvarHash(1019, true, 611, 10000);
         System.out.println(testHash.probeAndResults());
-        System.out.println("80%");
+        System.out.print("\n\n80%");
         testHash = new AlvarHash(1019, false, 819, 10000);
         System.out.println(testHash.probeAndResults());
         testHash = new AlvarHash(1019, true, 819, 10000);
